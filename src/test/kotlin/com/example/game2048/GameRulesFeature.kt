e535fcc7d241199d25b4a2c1d9d4f117a0ced327
Alvaro Garcia <derecerca@gmail.com>
2018-09-16 11:41:55 +0200

    Refactor: prefer not to depend on a static variable (i.e., global state)

--- src/test/kotlin/com/example/game2048/GameRulesFeature.kt
+++ src/test/kotlin/com/example/game2048/GameRulesFeature.kt
@@ -8,57 +8,55 @@ class GameRulesFeature {
     @Test
     fun `collapse two pieces that are together, reducing from two tiles to one`() {
         setMove(Board::left, Board::right) {
-            assertThat(Board(together(1, 1)).move()).isEqualTo(Board(single(2)))
+            assertThat(Board(together(1, 1)).move(it)).isEqualTo(Board(single(2)))
         }
     }
-
 
     @Test
     fun `collapse two pieces that are together, increasing the tile value`() {
         setMove(Board::left, Board::right) {
-            assertThat(Board(together(2, 2)).move()).isEqualTo(Board(single(3)))
+            assertThat(Board(together(2, 2)).move(it)).isEqualTo(Board(single(3)))
         }
     }
-
-
+    
     @Test
     fun `collapse two pieces that are together, keeps the rest of the tiles`() {
         setMove(Board::left, Board::right) {
-            assertThat(Board(together(2, 2, 1)).move()).isEqualTo(Board(together(3, 1)))
+            assertThat(Board(together(2, 2, 1)).move(it)).isEqualTo(Board(together(3, 1)))
         }
     }
 
     @Test
     fun `collapse two pieces that are together, collapses only in pairs`() {
         setMove(Board::left) {
-            assertThat(Board(together(2, 2, 2, 1)).move()).isEqualTo(Board(together(3, 2, 1)))
+            assertThat(Board(together(2, 2, 2, 1)).move(it)).isEqualTo(Board(together(3, 2, 1)))
         }
         setMove(Board::right) {
-            assertThat(Board(together(2, 2, 2, 1)).move()).isEqualTo(Board(together(2, 3, 1)))
+            assertThat(Board(together(2, 2, 2, 1)).move(it)).isEqualTo(Board(together(2, 3, 1)))
         }
     }
 
     @Test
     fun `collapse two pieces that are together, collapses multiple pairs at once`() {
         setMove(Board::left) {
-            assertThat(Board(together(2, 2, 2, 2)).move()).isEqualTo(Board(together(3, 3)))
-            assertThat(Board(together(2, 2, 3, 3)).move()).isEqualTo(Board(together(3, 4)))
+            assertThat(Board(together(2, 2, 2, 2)).move(it)).isEqualTo(Board(together(3, 3)))
+            assertThat(Board(together(2, 2, 3, 3)).move(it)).isEqualTo(Board(together(3, 4)))
         }
     }
 
     @Test
     fun `collapse two pieces that are together to the right, collapses multiple pairs at once`() {
         setMove(Board::left, Board::right) {
-            assertThat(Board(together(2, 2, 2, 2)).move()).isEqualTo(Board(together(3, 3)))
-            assertThat(Board(together(2, 2, 3, 3)).move()).isEqualTo(Board(together(3, 4)))
+            assertThat(Board(together(2, 2, 2, 2)).move(it)).isEqualTo(Board(together(3, 3)))
+            assertThat(Board(together(2, 2, 3, 3)).move(it)).isEqualTo(Board(together(3, 4)))
         }
     }
 
     @Test
     fun `collapse two pieces that are together to the right, collapses only in pairs, the first pieces to collapse are the ones on the right`() {
         setMove(Board::right) {
-            assertThat(Board(together(2, 2, 2, 2, 1)).move()).isEqualTo(Board(together(3, 3, 1)))
-            assertThat(Board(together(2, 2, 2, 2, 1, 3, 3)).move()).isEqualTo(Board(together(3, 3, 1, 4)))
+            assertThat(Board(together(2, 2, 2, 2, 1)).move(it)).isEqualTo(Board(together(3, 3, 1)))
+            assertThat(Board(together(2, 2, 2, 2, 1, 3, 3)).move(it)).isEqualTo(Board(together(3, 3, 1, 4)))
         }
     }
 
@@ -70,18 +68,13 @@ class GameRulesFeature {
         return values.map { Tile(it) }
     }
 
-    companion object Config {
-        lateinit var _move: (Board) -> Board
-
-        private fun setMove(vararg moves: (Board) -> Board, function: () -> Unit) {
-            moves.map {
-                Config._move = it
-                function.invoke()
-            }
+    private fun setMove(vararg moves: (Board) -> Board, function: ((Board) -> Board) -> Unit) {
+        moves.map {
+            function.invoke(it)
         }
     }
 }
 
-private fun Board.move(): Board {
-    return GameRulesFeature.Config._move.invoke(this)
+private fun Board.move(f: (Board) -> Board): Board {
+    return f.invoke(this)
 }
