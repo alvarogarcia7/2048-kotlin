ddc7f36b8d9eafa0d18532995d13f4623008e900
Alvaro Garcia <derecerca@gmail.com>
2018-09-16 11:22:17 +0200

    Refactor: introduce extension method to remove duplication and to ease copy-paste

--- src/test/kotlin/com/example/game2048/GameRulesFeature.kt
+++ src/test/kotlin/com/example/game2048/GameRulesFeature.kt
@@ -4,43 +4,53 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class GameRulesFeature {
+
     @Test
     fun `collapse two pieces that are together, reducing from two tiles to one`() {
-        assertThat(Board(together(1, 1)).left()).isEqualTo(Board(single(2)))
+        setMove(Board::left)
+        assertThat(Board(together(1, 1)).move()).isEqualTo(Board(single(2)))
     }
+
 
     @Test
     fun `collapse two pieces that are together, increasing the tile value`() {
-        assertThat(Board(together(2, 2)).left()).isEqualTo(Board(single(3)))
+        setMove(Board::left)
+        assertThat(Board(together(2, 2)).move()).isEqualTo(Board(single(3)))
     }
+
 
     @Test
     fun `collapse two pieces that are together, keeps the rest of the tiles`() {
-        assertThat(Board(together(2, 2, 1)).left()).isEqualTo(Board(together(3, 1)))
+        setMove(Board::left)
+        assertThat(Board(together(2, 2, 1)).move()).isEqualTo(Board(together(3, 1)))
     }
 
     @Test
     fun `collapse two pieces that are together, collapses only in pairs`() {
-        assertThat(Board(together(2, 2, 2, 1)).left()).isEqualTo(Board(together(3, 2, 1)))
+        setMove(Board::left)
+        assertThat(Board(together(2, 2, 2, 1)).move()).isEqualTo(Board(together(3, 2, 1)))
     }
 
     @Test
     fun `collapse two pieces that are together, collapses multiple pairs at once`() {
-        assertThat(Board(together(2, 2, 2, 2)).left()).isEqualTo(Board(together(3, 3)))
-        assertThat(Board(together(2, 2, 3, 3)).left()).isEqualTo(Board(together(3, 4)))
+        setMove(Board::left)
+        assertThat(Board(together(2, 2, 2, 2)).move()).isEqualTo(Board(together(3, 3)))
+        assertThat(Board(together(2, 2, 3, 3)).move()).isEqualTo(Board(together(3, 4)))
     }
 
     @Test
     fun `collapse two pieces that are together to the right, collapses multiple pairs at once`() {
-        assertThat(Board(together(2, 2, 2, 2)).right()).isEqualTo(Board(together(3, 3)))
-        assertThat(Board(together(2, 2, 3, 3)).right()).isEqualTo(Board(together(3, 4)))
+        setMove(Board::right)
+        assertThat(Board(together(2, 2, 2, 2)).move()).isEqualTo(Board(together(3, 3)))
+        assertThat(Board(together(2, 2, 3, 3)).move()).isEqualTo(Board(together(3, 4)))
     }
 
     @Test
     fun `collapse two pieces that are together to the right, collapses only in pairs, the first pieces to collapse are the ones on the right`() {
-        assertThat(Board(together(2, 2, 2, 1)).right()).isEqualTo(Board(together(2, 3, 1)))
-        assertThat(Board(together(2, 2, 2, 2, 1)).right()).isEqualTo(Board(together(3, 3, 1)))
-        assertThat(Board(together(2, 2, 2, 2, 1, 3, 3)).right()).isEqualTo(Board(together(3, 3, 1, 4)))
+        setMove(Board::right)
+        assertThat(Board(together(2, 2, 2, 1)).move()).isEqualTo(Board(together(2, 3, 1)))
+        assertThat(Board(together(2, 2, 2, 2, 1)).move()).isEqualTo(Board(together(3, 3, 1)))
+        assertThat(Board(together(2, 2, 2, 2, 1, 3, 3)).move()).isEqualTo(Board(together(3, 3, 1, 4)))
     }
 
     private fun single(value: Int): List<Tile> {
@@ -50,4 +60,16 @@ class GameRulesFeature {
     private fun together(vararg values: Int): List<Tile> {
         return values.map { Tile(it) }
     }
+
+    companion object Config {
+        lateinit var _move: (Board) -> Board
+
+        private fun setMove(move: (Board) -> Board) {
+            Config._move = move
+        }
+    }
+}
+
+private fun Board.move(): Board {
+    return GameRulesFeature.Config._move.invoke(this)
 }
