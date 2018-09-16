8ae3a61cff30a458779cbdbde3a07e90a3e3c62f
Alvaro Garcia <derecerca@gmail.com>
2018-09-16 17:03:21 +0200

    Refactor: remove dead code

--- src/main/kotlin/com/example/game2048/Board.kt
+++ src/main/kotlin/com/example/game2048/Board.kt
@@ -29,32 +29,6 @@ data class Board(val together: List<Tile>) {
         return Board(newBoard)
     }
 
-    private fun toUp(tiles: List<Tile>): List<Tile> {
-        return toLeft(tiles)
-    }
-
-    private fun toLeft(tiles: List<Tile>): List<Tile> {
-        var result = tiles.toMutableList()
-        var i = 0
-        val columns = Math.min(4, tiles.size)
-        val rows = Math.min(4, tiles.size / 4)
-        val single = fun(i: Int, j: Int) = i * columns + j
-        while (i < columns) {
-            var j = 0
-            while (j < rows) {
-                try {
-                    val tmp = tiles[single(i, j)]
-                    result[single(i, j)] = tiles[single(j, i)]
-                    result[single(j, i)] = tmp
-                } catch (e: IndexOutOfBoundsException) {
-                }
-                j++
-            }
-            i++
-        }
-        return result
-    }
-
     private fun moveLeft(tiles: MutableList<Tile>): List<Tile> {
         var result = tiles
         var i = 1
