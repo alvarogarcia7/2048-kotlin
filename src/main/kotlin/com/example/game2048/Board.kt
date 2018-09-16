694dd8a161333bab2d51206fdf273a4b7d8995f8
Alvaro Garcia <derecerca@gmail.com>
2018-09-16 17:51:07 +0200

    Save WIP: halfway through making spaces when moving to the right

--- src/main/kotlin/com/example/game2048/Board.kt
+++ src/main/kotlin/com/example/game2048/Board.kt
@@ -1,46 +1,64 @@
 package com.example.game2048
 
-data class Board(val together: List<Tile>) {
+data class Board(val together: List<List<Tile>>) {
     fun left(): Board {
-        return Board(moveLeft(together.toMutableList()))
+        return Board(moveLeft(together))
+    }
+
+    private fun padWithEmptyTiles(values: List<Tile>, desiredSize: Int): MutableList<Tile> {
+        val result = values.toMutableList()
+        for (i in values.size until desiredSize) {
+            result.add(Tile(0))
+        }
+        return result
     }
 
     fun right(): Board {
-        return Board(moveLeft(together.asReversed().toMutableList()).asReversed())
+        return Board(reverseIntraRows(moveLeft(reverseIntraRows(together))))
+    }
+
+    private fun reverseIntraRows(tiles: List<List<Tile>>): List<List<Tile>> {
+        return tiles.map { it.asReversed() }
     }
 
     fun up(): Board {
-        var result = (together).toMutableList()
+        var result = makeMutable(together)
         val columns = Math.min(4, result.size)
         val rows = Math.min(4, result.size / 4)
-        val single = fun(i: Int, j: Int) = i * columns + j
-        for (i in 0..columns) {
-            for (j in 0..rows) {
+        for (i in 0 until columns) {
+            for (j in 0 until rows) {
                 try {
-                    if (result[single(i, j)] == result[single(i - 1, j)] && result[single(i, j)] != Tile(0)) {
-                        result[single(i - 1, j)] = result[single(i - 1, j)].next()
-                        result.removeAt(single(i, j))
+                    if (result[i][j] == result[i - 1][j] && result[i][j] != Tile(0)) {
+                        result[i - 1][j] = result[i - 1][j].next()
+                        result[i].removeAt(j)
                     }
                 } catch (e: IndexOutOfBoundsException) {
                 }
             }
+            result[i] = padWithEmptyTiles(result[i], together[i].size)
         }
-        val newBoard = result
-        return Board(newBoard)
+        return Board(result)
     }
 
-    private fun moveLeft(tiles: MutableList<Tile>): List<Tile> {
-        var result = tiles
-        var i = 1
-        while (i < result.size) {
-            if (result[i] == result[i - 1] && result[i] != Tile(0)) {
-                result[i - 1] = result[i - 1].next()
-                result.removeAt(i)
+    private fun moveLeft(tiles: List<List<Tile>>): List<List<Tile>> {
+        var total: MutableList<MutableList<Tile>> = makeMutable(tiles)
+        for (i in 0 until total.size) {
+            var result: MutableList<Tile> = total[i]
+            var j = 1
+            while (j < result.size) {
+                if (result[j] == result[j - 1] && result[j] != Tile(0)) {
+                    result[j - 1] = result[j - 1].next()
+                    result.removeAt(j)
+                }
+                j++
             }
-            i++
+            total[i] = padWithEmptyTiles(result, 4)
         }
-        return result
+        return total
     }
+
+    private fun makeMutable(tiles: List<List<Tile>>) =
+            tiles.map { it.toMutableList() }.toMutableList()
 
 }
 
--- src/test/kotlin/com/example/game2048/GameRulesFeature.kt
+++ src/test/kotlin/com/example/game2048/GameRulesFeature.kt
@@ -8,72 +8,127 @@ class GameRulesFeature {
     @Test
     fun `collapse two pieces that are together, reducing from two tiles to one`() {
         setMove(Board::left, Board::right) {
-            assertThat(Board(together(1, 1)).move(it)).isEqualTo(Board(single(2)))
+            assertThat(Board(singleRow(1, 1)).move(it)).isEqualTo(Board(single(2)))
+        }
+        setMove(Board::right) {
+            assertThat(Board(singleRow(1, 1)).move(it)).isEqualTo(Board(singleRow(0, 0, 0, 2)))
         }
     }
 
     @Test
     fun `collapse two pieces that are together in the same column, when moving columns, reducing from two tiles to one`() {
         setMove(Board::up) {
-            assertThat(Board(together(
-                    1, 0, 0, 0,
-                    1)).move(it)).isEqualTo(Board(together(2, 0, 0, 0)))
+            assertThat(Board(single(
+                    row(1, 0, 0, 0),
+                    row(1))).move(it)).isEqualTo(Board(singleRow(2, 0, 0, 0)))
         }
     }
 
     @Test
-    fun `do not collapse two pieces that are together in the same row when moving in columns`() {
+    fun `do not collapse two pieces that are together in the same singleRow when moving in columns`() {
         setMove(Board::up) {
-            assertThat(Board(together(1, 1)).move(it)).isEqualTo(Board(together(1, 1)))
+            assertThat(Board(singleRow(1, 1)).move(it)).isEqualTo(Board(singleRow(1, 1)))
         }
     }
 
     @Test
     fun `collapse two pieces that are together, increasing the tile value`() {
-        setMove(Board::left, Board::right) {
-            assertThat(Board(together(2, 2)).move(it)).isEqualTo(Board(single(3)))
+        setMove(Board::left) {
+            assertThat(Board(singleRow(2, 2)).move(it)).isEqualTo(Board(single(3)))
+        }
+        setMove(Board::right) {
+            assertThat(Board(singleRow(2, 2)).move(it)).isEqualTo(Board(singleRow(0, 0, 0, 3)))
         }
     }
 
     @Test
     fun `collapse two pieces that are together, keeps the rest of the tiles`() {
-        setMove(Board::left, Board::right) {
-            assertThat(Board(together(2, 2, 1)).move(it)).isEqualTo(Board(together(3, 1)))
+        setMove(Board::left) {
+            assertThat(Board(singleRow(2, 2, 1)).move(it)).isEqualTo(Board(singleRow(3, 1)))
+        }
+        setMove(Board::right) {
+            assertThat(Board(singleRow(2, 2, 1)).move(it)).isEqualTo(Board(singleRow(0, 0, 3, 1)))
         }
     }
 
     @Test
     fun `collapse two pieces that are together, collapses only in pairs`() {
         setMove(Board::left) {
-            assertThat(Board(together(2, 2, 2, 1)).move(it)).isEqualTo(Board(together(3, 2, 1)))
+            assertThat(Board(singleRow(2, 2, 2, 1)).move(it)).isEqualTo(Board(singleRow(3, 2, 1)))
         }
         setMove(Board::right) {
-            assertThat(Board(together(2, 2, 2, 1)).move(it)).isEqualTo(Board(together(2, 3, 1)))
+            assertThat(Board(singleRow(2, 2, 2, 1)).move(it)).isEqualTo(Board(singleRow(0, 2, 3, 1)))
         }
     }
 
     @Test
     fun `collapse two pieces that are together, collapses multiple pairs at once`() {
-        setMove(Board::left, Board::right) {
-            assertThat(Board(together(2, 2, 2, 2)).move(it)).isEqualTo(Board(together(3, 3)))
-            assertThat(Board(together(2, 2, 3, 3)).move(it)).isEqualTo(Board(together(3, 4)))
+        setMove(Board::left) {
+            assertThat(Board(singleRow(2, 2, 2, 2)).move(it)).isEqualTo(Board(singleRow(3, 3)))
+            assertThat(Board(singleRow(2, 2, 3, 3)).move(it)).isEqualTo(Board(singleRow(3, 4)))
+        }
+        setMove(Board::right) {
+            assertThat(Board(singleRow(2, 2, 2, 2)).move(it)).isEqualTo(Board(singleRow(0, 0, 3, 3)))
+            assertThat(Board(singleRow(2, 2, 3, 3)).move(it)).isEqualTo(Board(singleRow(0, 0, 3, 4)))
         }
     }
 
     @Test
     fun `collapse two pieces that are together, collapses only in pairs, the first pieces to collapse are the ones on the direction of the move`() {
-        setMove(Board::left, Board::right) {
-            assertThat(Board(together(2, 2, 2, 2, 1)).move(it)).isEqualTo(Board(together(3, 3, 1)))
-            assertThat(Board(together(2, 2, 2, 2, 1, 3, 3)).move(it)).isEqualTo(Board(together(3, 3, 1, 4)))
+        setMove(Board::left) {
+            assertThat(Board(single(row(2, 2, 2, 2), row(1))).move(it)).isEqualTo(Board(single(row(3, 3, 0, 0), row(1))))
+            assertThat(Board(single(row(2, 2, 2, 2), row(1, 3, 3))).move(it)).isEqualTo(Board(single(row(3, 3, 0, 0), row(1, 4))))
+        }
+        setMove(Board::right) {
+            assertThat(Board(single(row(2, 2, 2, 2), row(1))).move(it)).isEqualTo(Board(single(row(0, 0, 3, 3), row(0, 0, 0, 1))))
+            assertThat(Board(single(row(2, 2, 2, 2), row(1, 3, 3))).move(it)).isEqualTo(Board(single(row(0, 0, 3, 3), row(0, 0, 1, 4))))
         }
     }
 
-    private fun single(value: Int): List<Tile> {
-        return arrayListOf(Tile(value))
+    private fun row(vararg values: Int): List<Tile> {
+        return padWithEmptyTiles(values.map { Tile(it) }, 4)
     }
 
-    private fun together(vararg values: Int): List<Tile> {
-        return values.map { Tile(it) }
+    private fun single(elements: List<Tile>): List<List<Tile>> {
+        return listOf(
+                elements,
+                emptyRow(),
+                emptyRow(),
+                emptyRow()
+        )
+    }
+
+    private fun single(elements: List<Tile>, elements2: List<Tile>): List<List<Tile>> {
+        return listOf(
+                elements,
+                elements2,
+                emptyRow(),
+                emptyRow()
+        )
+    }
+
+    private fun single(value: Int): List<List<Tile>> {
+        val elements = padWithEmptyTiles(arrayListOf(Tile(value)), 4)
+        return single(elements)
+    }
+
+    private fun emptyRow(): List<Tile> {
+        return padWithEmptyTiles(emptyList(), 4)
+    }
+
+    private fun singleRow(vararg values: Int): List<List<Tile>> {
+        assertThat(values.size).isLessThanOrEqualTo(4)
+        val interestingPart = values.map { Tile(it) }
+        return single(padWithEmptyTiles(interestingPart, 4))
+    }
+
+    private fun padWithEmptyTiles(interestingPart: List<Tile>, desiredSize: Int): List<Tile> {
+        val result = interestingPart.toMutableList()
+        for (i in interestingPart.size until desiredSize) {
+            result.add(Tile(0))
+        }
+        assertThat(result).hasSize(desiredSize)
+        return result
     }
 
     private fun setMove(vararg moves: (Board) -> Board, function: ((Board) -> Board) -> Unit) {
