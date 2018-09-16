a62250cb4ad6b97805b3cdf3c54a4faa7f54b795
Alvaro Garcia <derecerca@gmail.com>
2018-09-16 10:41:24 +0200

    Refactor: replace algorithm - prefer functional style

--- src/main/kotlin/com/example/game2048/Board.kt
+++ src/main/kotlin/com/example/game2048/Board.kt
@@ -2,10 +2,13 @@ package com.example.game2048
 
 data class Board(val together: List<Tile>) {
     fun left(): Board {
-        return Board(if (together.size == 3 && together[0] == together[1]) {
-            listOf(together.first().next(), together.last())
-        } else {
-            listOf(together.first().next())
+
+        return Board(together.zipWithNext().flatMap { tile ->
+            if (tile.first == tile.second) {
+                listOf(tile.first.next())
+            } else {
+                listOf(tile.second)
+            }
         })
     }
 
